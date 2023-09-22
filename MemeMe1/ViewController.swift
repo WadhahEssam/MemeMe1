58816f49adfe5051e09ec0ded5e945f68fc4009e
Wadah Esam <wadah-wleed@hotmail.com>
2023-09-22 03:18:35 +0300

    finishing the style of the top and bottom texts

--- MemeMe1/ViewController.swift
+++ MemeMe1/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupMemeTextFields()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
+    private func setupMemeTextFields() {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.clear,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.backgroundColor: UIColor.clear,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 50)!,
+            NSAttributedString.Key.strokeWidth: -2,
+            NSAttributedString.Key.paragraphStyle: paragraphStyle
+        ]
+
+        [topTextField, bottomTextField].forEach { textField in
+            textField.defaultTextAttributes = memeTextAttributes
+            textField.backgroundColor = UIColor.clear
+            textField.borderStyle = .none
+        }
+    }
 
 }
 
